5f7adda183e3704ba2adb03aeb79b9a1e68186bd
Kássio Silva <Kassio.Sa@ibm.com>
2023-11-25 19:44:16 -0300

    feat: iniciando estrutura do projeto

---
+++ src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+    private final String marca;
+    private final String modelo;
+    private final int ano;
+
+    public Carro(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+}
